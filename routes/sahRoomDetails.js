641258b276181efcf4433619bd6e5b54e38c96a0
yash17ys4 <yash17.ys4@gmail.com>
2020-11-03 20:01:45 +0530

    rooms CRUD complete

--- routes/sahRoomDetails.js
+++ routes/sahRoomDetails.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { body, validationResult } = require('express-validator');
+const { body, validationResult, sanitizeParam } = require('express-validator');
 const sahRoomDetailsControllers = require('../controllers/sahRoomDetailsComtrollers');
 
 const router = express.Router();
 
 router.get('/', sahRoomDetailsControllers.getDetails);
+
 router.post('/create', [
     body('building').notEmpty(),
     body('floor').notEmpty(),
@@ -14,12 +15,12 @@ router.post('/create', [
     body('blocked').notEmpty(),
     body('remark').notEmpty(),
     body('maxchecked').notEmpty()
-
 ], sahRoomDetailsControllers.createDetails);
 
-router.put('/update', sahRoomDetailsControllers.updateDetails);
-router.delete('/delete', [
-    body('room_no').notEmpty()
-], sahRoomDetailsControllers.deleteDetails);
+router.put('/update/:room_no', [
+    body('blocked').notEmpty()
+], sahRoomDetailsControllers.updateDetails);
+
+router.delete('/delete/:room_no', sahRoomDetailsControllers.deleteDetails);
 
 module.exports = router;
